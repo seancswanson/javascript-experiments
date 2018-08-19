eaacaa9f94b9a7b424257a5f0714842d34548433
Sean Swanson <sean-swan@hotmail.com>
2018-08-19 11:40:02 -0700

    Playing with starter code.

--- hoisting/script.js
+++ hoisting/script.js
@@ -2,29 +2,29 @@
 // Lecture: Hoisting
 
 // functions
-calculateAge(1994);
+// calculateAge(1994);
+//
+// function calculateAge(year) {
+//   console.log(2018 - year);
+// }
+//
+// var retirement = function(year) {
+//   console.log(65 - (2016 - year));
+// }
 
-function calculateAge(year) {
-  console.log(2018 - year);
-}
-
-var retirement = function(year) {
-  console.log(65 - (2016 - year));
-}
-
-retirement(1994);
+// retirement(1994);
 
 // variables
-
-var age = 23;
-console.log(age);
-
-function foo() {
-  var age = 24;
-  console.log(age, 'will be different');
-}
-foo();
-console.log(age);
+//
+// var age = 23;
+// console.log(age);
+//
+// function foo() {
+//   var age = 24;
+//   console.log(age, 'will be different');
+// }
+// foo();
+// console.log(age);
 
 
 
@@ -37,44 +37,43 @@ console.log(age);
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
+//
+// function first() {
+//     var b = 'Hi!';
+//     second();
+//
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + b + c);
+//     }
+// }
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
-var a = 'Hello!';
-first();
-
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
-
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
+// var a = 'Hello!';
+// first();
+//
+// function first() {
+//     var b = 'Hi!';
+//     second();
+//
+//     function second() {
+//         var c = 'Hey!';
+//         third()
+//     }
+// }
+//
+// function third() {
+//     var d = 'John';
+//     console.log(a + d);
+// }
+//
 
 
 
