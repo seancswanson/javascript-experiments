c353ec7ede4c0c97ef186eb567bc0b054f930d9f
Sean Swanson <sean-swan@hotmail.com>
2018-09-02 11:48:09 -0700

    More proto experiments

--- advance-js/script.js
+++ advance-js/script.js
@@ -5,3 +5,31 @@ var sean = {
   yearOfBirth: 1994,
   job: 'developer'
 };
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2018 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var sean = new Person('Sean', 1994, 'developer');
+
+var jane = new Person('Jane', 1962, 'Designer');
+
+var mark = new Person('Mark', 1955, 'Ops');
+
+sean.calculateAge()
+
+jane.calculateAge();
+
+mark.calculateAge();
+
+console.log(sean.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
