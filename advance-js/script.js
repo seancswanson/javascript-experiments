f00ca33db0d4cf3e1131d03ce466e564af289113
Sean Swanson <sean-swan@hotmail.com>
2018-09-03 11:19:04 -0700

    Callback cardio

--- advance-js/script.js
+++ advance-js/script.js
@@ -38,19 +38,88 @@
 
 // Object.create
 
-var personProto = {
-  calculateAge: function() {
-    console.log(2018 - this.yearOfBirth);
-  }
+// var personProto = {
+//   calculateAge: function() {
+//     console.log(2018 - this.yearOfBirth);
+//   }
+// }
+//
+// var sean = Object.create(personProto);
+// sean.name = 'sean';
+// sean.yearOfBirth = 1994;
+// sean.job = 'dev';
+//
+// var jane = Object.create(personProto, {
+//   name: { value: 'Jane'},
+//   yearOfBirth: { value: 1979},
+//   job: { value: 'Designer'},
+// });
+
+//  Primitives vs  Objects
+
+
+// Primitives
+var a = 42;
+var b = a;
+
+a = 46;
+
+// console.log(a, b);
+
+
+// Objects
+var obj1 = {
+  name: 'John',
+  age: 26
 }
 
-var sean = Object.create(personProto);
-sean.name = 'sean';
-sean.yearOfBirth = 1994;
-sean.job = 'dev';
+var obj2 = obj1;
 
-var jane = Object.create(personProto, {
-  name: { value: 'Jane'},
-  yearOfBirth: { value: 1979},
-  job: { value: 'Designer'},
-});
+obj1.age = 30;
+
+// console.log(obj1.age);
+// console.log(obj2.age);
+
+// Functions
+
+// var age = 24;
+// var obj = {
+//   name: 'Sean',
+//   city: 'Seattle'
+// };
+//
+// function change(a, b) {
+//   a = 30;
+//   b.city = 'San Fran';
+// }
+//
+//
+// change(age, obj)
+//
+// console.log(age)
+// console.log(obj.city)
+
+// Passing functions as arguments
+
+var years = [1994, 1993, 1292, 2005, 2002];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+
+  return arrRes;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function calculateAge(el) {
+  return 2018 - el;
+}
+
+// console.log(arrayCalc(years, calculateAge))
+console.log(arrayCalc(years, isFullAge))
